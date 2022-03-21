be6e4949fe58e38a28d2c6edec8a1f17aa473cc9
Jelle Luijkx <j.d.luijkx@tudelft.nl>
2022-03-21 07:12:44 +0100

    chore: add eagerx_ode code with tests and ci

---
+++ eagerx_ode/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.2"
+import eagerx_ode.bridge  # noqa: F401
+import eagerx_ode.enginenodes  # noqa: F401
+import eagerx_ode.enginestates  # noqa: F401
+import eagerx_ode.pendulum.objects  # noqa: F401
---
+++ eagerx_ode/pendulum/objects.py
@@ -0,0 +1,131 @@
+# ROS IMPORTS
+from std_msgs.msg import Float32MultiArray
+
+# EAGERx IMPORTS
+from eagerx_ode.bridge import OdeBridge
+from eagerx.core.entities import (
+    Object,
+    EngineNode,
+    SpaceConverter,
+    EngineState,
+)
+from eagerx.core.specs import ObjectSpec
+from eagerx.core.graph_engine import EngineGraph
+import eagerx.core.register as register
+
+
+class Pendulum(Object):
+    entity_id = "Eagerx_Ode_Pendulum"
+
+    @staticmethod
+    @register.sensors(pendulum_output=Float32MultiArray, action_applied=Float32MultiArray)
+    @register.actuators(pendulum_input=Float32MultiArray)
+    @register.simstates(model_state=Float32MultiArray)
+    @register.config()
+    def agnostic(spec: ObjectSpec, rate):
+        """Agnostic definition of the pendulum"""
+        # Register standard converters, space_converters, and processors
+        import eagerx.converters  # noqa # pylint: disable=unused-import
+
+        # Set observation properties: (space_converters, rate, etc...)
+        spec.sensors.pendulum_output.rate = rate
+        spec.sensors.pendulum_output.space_converter = SpaceConverter.make(
+            "Space_Float32MultiArray", low=[-3.14, -9], high=[3.14, 9], dtype="float32"
+        )
+
+        spec.sensors.action_applied.rate = rate
+        spec.sensors.action_applied.space_converter = SpaceConverter.make(
+            "Space_Float32MultiArray", low=[-3], high=[3], dtype="float32"
+        )
+
+        # Set actuator properties: (space_converters, rate, etc...)
+        spec.actuators.pendulum_input.rate = rate
+        spec.actuators.pendulum_input.window = 1
+        spec.actuators.pendulum_input.space_converter = SpaceConverter.make(
+            "Space_Float32MultiArray", low=[-3], high=[3], dtype="float32"
+        )
+
+        # Set model_state properties: (space_converters)
+        spec.states.model_state.space_converter = SpaceConverter.make(
+            "Space_Float32MultiArray",
+            low=[-3.14159265359, -9],
+            high=[3.14159265359, 9],
+            dtype="float32",
+        )
+
+    @staticmethod
+    @register.spec(entity_id, Object)
+    def spec(spec: ObjectSpec, name: str, sensors=None, states=None, rate=30):
+        """Object spec of pendulum"""
+        # Performs all the steps to fill-in the params with registered info about all functions.
+        Pendulum.initialize_spec(spec)
+
+        # Modify default agnostic params
+        # Only allow changes to the agnostic params (rates, windows, (space)converters, etc...
+        spec.config.name = name
+        spec.config.sensors = (
+            sensors if sensors else ["pendulum_output", "action_applied"]
+        )
+        spec.config.actuators = ["pendulum_input"]
+        spec.config.states = states if states else ["model_state"]
+
+        # Add bridge implementation
+        Pendulum.agnostic(spec, rate)
+
+    @staticmethod
+    @register.bridge(
+        entity_id, OdeBridge
+    )  # This decorator pre-initializes bridge implementation with default object_params
+    def ode_bridge(spec: ObjectSpec, graph: EngineGraph):
+        """Engine-specific implementation (OdeBridge) of the object."""
+        # Import any object specific entities for this bridge
+        import eagerx_ode.pendulum.pendulum_ode  # noqa # pylint: disable=unused-import
+
+        # Set object arguments (nothing to set here in this case)
+        spec.OdeBridge.ode = "eagerx_ode.pendulum.pendulum_ode/pendulum_ode"
+        # Set default params of pendulum ode [J, m, l, b0, K, R].
+        spec.OdeBridge.ode_params = [
+            0.000189238,
+            0.0563641,
+            0.0437891,
+            0.000142205,
+            0.0502769,
+            9.83536,
+        ]
+
+        # Create simstates (no agnostic states defined in this case)
+        spec.OdeBridge.states.model_state = EngineState.make("OdeSimState")
+
+        # Create sensor engine nodes
+        obs = EngineNode.make(
+            "OdeOutput",
+            "pendulum_output",
+            rate=spec.sensors.pendulum_output.rate,
+            process=2,
+        )
+
+        # Create actuator engine nodes
+        action = EngineNode.make(
+            "OdeInput",
+            "pendulum_actuator",
+            rate=spec.actuators.pendulum_input.rate,
+            process=2,
+            default_action=[0],
+        )
+
+        # Connect all engine nodes
+        graph.add([obs, action])
+        graph.connect(source=obs.outputs.observation, sensor="pendulum_output")
+        graph.connect(actuator="pendulum_input", target=action.inputs.action)
+
+        # Add action applied
+        applied = EngineNode.make(
+            "ActionApplied", "applied", rate=spec.sensors.action_applied.rate, process=2
+        )
+        graph.add(applied)
+        graph.connect(
+            source=action.outputs.action_applied,
+            target=applied.inputs.action_applied,
+            skip=True,
+        )
+        graph.connect(source=applied.outputs.action_applied, sensor="action_applied")
